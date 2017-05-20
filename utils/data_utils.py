c85675d7ee43f42683fc2f179e444f9f55135813
zklgame <chikanebest@163.com>
2017-05-21 00:27:51 +0800

    add visualization-pytorch

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -88,3 +88,30 @@ def get_CIFAR10_data(ROOT, num_training=49000, num_validation=1000, num_test=100
     # X_test -= mean_image
 
     return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def load_imagenet_val(num=None):
+    """
+    Load a handful of validation images from ImageNet.
+
+    Inputs:
+    - num: Number of images to load (max of 25)
+
+    Returns:
+    - X: numpy array with shape [num, 224, 224, 3]
+    - y: numpy array of integer image labels, shape [num]
+    - class_names: dict mapping integer label to class name
+    """
+    imagenet_fn = 'datasets/imagenet_val_25.npz'
+    if not os.path.isfile(imagenet_fn):
+        print('file %s not found' % imagenet_fn)
+        assert False, 'Need to download imagenet_val_25.npz in datasets folder'
+    f = np.load(imagenet_fn)
+    X = f['X']
+    y = f['y']
+    class_names = f['label_map'].item()
+    if num is not None:
+        X = X[:num]
+        y = y[:num]
+
+    return X, y, class_names
--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import urllib2
 import tempfile
 from scipy.misc import imread
@@ -20,3 +22,7 @@ def image_from_url(url):
         print('URL Error: ', e.reason, url)
     except urllib2.HTTPError as e:
         print('HTTP Error: ', e.code, url)
+
+
+SQUEEZENET_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+SQUEEZENET_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
